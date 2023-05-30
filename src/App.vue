5970762a499e5ffd5bd8e91507d9fd3c73f6637b
v_beileiliu <v_beileiliu@tencent.com>
2023-05-30 09:59:57 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import VirtualScroll from "./VirtualScroll.vue";
+const time = ref(0);
+let start = new Date();
+onMounted(() => {
+  time.value = new Date() - start;
+});
+</script>
+
+<template>
+  <section>
+    <header>本次渲染花费 <b>{{ time }}</b> ms</header>
+    <VirtualScroll />
+  </section>
+</template>
+
+<style>
+#app {
+  position: absolute;
+  top: 200px;
+  left: 200px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
